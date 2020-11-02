53dce85b6b02abbc3cf59613c60cb725fcf29eec
diana maina <dianamainachui@gmail.com>
2020-11-01 22:03:17 -0500

    added routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,32 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
-
-const PORT = 3000;
-
 const app = express();
 
-app.use(logger("dev"));
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
 });
 
+// app.use(logger("dev"));
+
+// app.use(compression());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("public"));
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on http://localhost${PORT}`);
 });
