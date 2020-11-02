b6e92e250fbc13697286fbb0962390486019a0c6
diana maina <dianamainachui@gmail.com>
2020-11-01 22:08:54 -0500

    added middleware

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
